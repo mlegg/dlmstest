8839a9df4aa1bcfe120b2f15ab127785f674789a
michael.leggett1 <michael.leggett1@navy.mil>
2019-04-15 20:14:34 +0000

    Added overrides

--- src/main/java/MilstripParser.java
+++ src/main/java/MilstripParser.java
@@ -1,4 +1,13 @@
-//
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class MilstripParser  implements Parser {
     private static final int SIZE = 80;
     
@@ -6,9 +15,32 @@ public class MilstripParser  implements Parser {
         
     }
     
+    @Override
     public MILS parse(String source) throws FormatException {
         if (source.length() < SIZE) {
             throw new FormatException();
+            
         }
+        MILS milstrip = null;
+        return milstrip;
+        
     }
+    
+    @Override
+	public MILS parse(File fileName) throws FormatException, IOException {
+	     MILS milstrip = null;
+	    return milstrip;
+    }
+    
+    //private Milstrip scanSource(Scanner scanner, Context context) {
+    //}
+    
+    @Override
+	public MILS parse(InputStream source) throws FormatException, IOException {
+	    
+	    MILS milstrip = null;
+	    return milstrip;
+    }
+
+    
 }
