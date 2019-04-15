34d980d42a70c649e0bc84cc740c0556afd494ad
michael.leggett1 <michael.leggett1@navy.mil>
2019-04-15 18:52:07 +0000

    Updated commit

--- src/main/java/FormatException.java
+++ src/main/java/FormatException.java
@@ -1,4 +1,4 @@
-
+//
 public class FormatException extends Exception {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-
+//
 public interface Parser {
     MILS parse(File source) throws FormatException, IOException;
 	MILS parse(String source) throws FormatException;
